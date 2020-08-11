6629d9ea40826badecfa7e3e411b8ac41313f2ef
Alexander Weber <awalexweber99@gmail.com>
2020-08-11 15:20:17 -0700

    Create MergeKSortedLists.py

---
+++ LeetCode/Solved/Hard/MergeKSortedLists.py
@@ -0,0 +1,39 @@
+'''
+23. Merge k Sorted Lists
+
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+Example:
+
+Input:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+Output: 1->1->2->3->4->4->5->6
+'''
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+from typing import List
+
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        # 'Brute Force' solution
+        # O(n*k) where n is the maximal length of a list and k is the number of lists
+        self.nodes = []
+        head = point = ListNode(0)
+        for l in lists:
+            while l:
+                self.nodes.append(l.val)
+                l = l.next
+        for x in sorted(self.nodes):
+            point.next = ListNode(x)
+            point = point.next
+        return head.next
+        
