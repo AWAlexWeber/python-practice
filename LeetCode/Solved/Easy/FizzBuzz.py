c173f63e6f0bb9e5a3e9006aab7b3afbd9c7c251
Alexander Weber <awalexweber99@gmail.com>
2020-12-01 17:51:15 -0800

    LC +1

---
+++ LeetCode/Solved/Easy/FizzBuzz.py
@@ -0,0 +1,17 @@
+''' Famous fizz buzz '''
+
+from typing import List
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        o = list()
+        for i in range(1,n + 1):
+            s = ''
+            if i % 3 == 0:
+                s += 'Fizz'
+            if i % 5 == 0:
+                s += 'Buzz'
+            elif i % 3 != 0 and i % 5 != 0:
+                s += str(i)
+            o.append(s)
+        return o
