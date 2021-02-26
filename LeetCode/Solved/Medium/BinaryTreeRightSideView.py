330f0b0b1d510a2869c8b503ef5f7ce1d72c551f
Alexander Weber <awalexweber99@gmail.com>
2021-02-26 13:31:52 -0800

    Minor LC modifications

--- LeetCode/Solved/Medium/BinaryTreeRightSideView.py
+++ LeetCode/Solved/Medium/BinaryTreeRightSideView.py
@@ -40,3 +40,26 @@ class Solution:
             
         self.r(node.right, h, depth + 1)
         self.r(node.left, h, depth + 1)
+
+
+class SolutionTwo:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        # Going to DFS, keeping track of how far right we are and our depth
+        # The node visible at later n is the node that is the furthest to the right that we've visisted.
+        # Dictionary to track node value and node distance to the right. Key is depth, value is (node value, distance)
+        visit = {}
+        
+        def dfs(node, depth=0, dist=0):
+            if depth not in visit or depth > visit[depth][1]:
+                visit[depth] = (node.val, dist)
+            if node.left:
+                dfs(node.left, depth + 1, dist - 1)
+            if node.right:
+                dfs(node.right, depth + 1, dist + 1)
+        
+        dfs(root)
+        output = [visit[key][0] for key in visit.keys()]
+        return output
+        
